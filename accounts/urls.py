be52e11bae0215bc0e7f78ce6306df03c943ab89
anointing adebayo <124325944+anointing1234@users.noreply.github.com>
2025-05-05 20:51:04 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('contacts_view/',views.contacts_view,name='contacts_view'),
     path('submit-comment/', views.submit_comment, name='submit_comment'),
     path('send-message/', views.send_message, name='send_message'),
+    path('book/',views.book_views,name="book"),
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
