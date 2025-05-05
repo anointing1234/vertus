e93c84813470a94b13eca9b4b915d553e4177539
anointing adebayo <124325944+anointing1234@users.noreply.github.com>
2025-05-05 10:02:26 +0100

    commted

--- vertus_hotels/settings.py
+++ vertus_hotels/settings.py
@@ -170,14 +170,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND      = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST         = 'smtp.zoho.eu'       # or smtp.zoho.com
-EMAIL_PORT         = 465
-EMAIL_USE_TLS      = False
-EMAIL_USE_SSL      = True
-EMAIL_HOST_USER    = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD= env('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = "yakubudestiny9@gmail.com"
+# EMAIL_BACKEND      = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST         = 'smtp.zoho.eu'       # or smtp.zoho.com
+# EMAIL_PORT         = 465
+# EMAIL_USE_TLS      = False
+# EMAIL_USE_SSL      = True
+# EMAIL_HOST_USER    = env('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD= env('EMAIL_HOST_PASSWORD')
+# DEFAULT_FROM_EMAIL = "yakubudestiny9@gmail.com"
 
 
 
